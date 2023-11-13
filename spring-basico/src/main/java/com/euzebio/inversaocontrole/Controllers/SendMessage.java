f2c4b598df7d3dcc2a4fd5f21656f93d629d1c90
Claudemir Ribeiro <clauribeirodevjava43@gmail.com>
2023-11-13 17:29:53 -0300

    deletado conteudo inversão de controle basico

--- spring-basico/src/main/java/com/euzebio/inversaocontrole/Controllers/SendMessage.java
+++
@@ -1,7 +0,0 @@
-package com.euzebio.inversaocontrole.Controllers;
-
-import com.euzebio.inversaocontrole.Models.User;
-
-public interface SendMessage {
-    public void sendMessage(User usuario, String message);
-}
