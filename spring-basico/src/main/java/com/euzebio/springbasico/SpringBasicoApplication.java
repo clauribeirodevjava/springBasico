4b407514077f6974e168a8646628a12bdf323a81
Claudemir Ribeiro <clauribeirodevjava43@gmail.com>
2023-11-13 14:06:30 -0300

    erro induzido para entendimento do bean unico

--- spring-basico/src/main/java/com/euzebio/springbasico/SpringBasicoApplication.java
+++ spring-basico/src/main/java/com/euzebio/springbasico/SpringBasicoApplication.java
@@ -1,7 +1,5 @@
 package com.euzebio.springbasico;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,20 +8,18 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBasicoApplication {
-	@Autowired
-	List<Oi> ois;
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBasicoApplication.class, args);
+    @Autowired
+    Oi oi;
 
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicoApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner initOi(){
-		return args -> {
-			ois.forEach(oi ->  {
-				System.out.println(oi.oi());
-			});
-		};
-	}
+    @Bean
+    CommandLineRunner initOi() {
+        return args -> {
+           System.out.println(oi.oi());
 
+        };
+    }
 }
