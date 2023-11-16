5b8d759955eea8cf2c61112d72f759128a611469
Claudemir Ribeiro <clauribeirodevjava43@gmail.com>
2023-11-16 13:26:34 -0300

    injeção por construtor

--- spring-basico/src/main/java/com/euzebio/springbasico/controller/OiController.java
+++ spring-basico/src/main/java/com/euzebio/springbasico/controller/OiController.java
@@ -1,6 +1,5 @@
 package com.euzebio.springbasico.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,26 +11,27 @@ import com.euzebio.springbasico.Oi;
 @RestController
 @RequestMapping("/oi")
 public class OiController {
-    
-    @Autowired
-    @Qualifier("helloPortuguese")
-    Oi helloOne;
-    
-    @Autowired
-    @Qualifier("helloEnglish")
-    Oi helloTwo;
-    
-    @Autowired
-    @Qualifier("helloEspanhol")
-    Oi helloThree;
 
-    @GetMapping("/cumprimentar")
-    @ResponseBody
-    public String cumprimentar() {
-        StringBuilder result = new StringBuilder();
-        result.append(helloOne.oi()).append("<br>");
-        result.append(helloTwo.oi()).append("<br>");
-        result.append(helloThree.oi()).append("<br>");
-        return result.toString();
-    }
+	private final Oi helloOne;
+	private final Oi helloTwo;
+	private final Oi helloThree;
+
+	public OiController(
+			@Qualifier("helloPortuguese") Oi helloOne,
+			@Qualifier("helloEnglish") Oi helloTwo,
+			@Qualifier("helloEspanhol") Oi helloThree) {
+		this.helloOne = helloOne;
+		this.helloTwo = helloTwo;
+		this.helloThree = helloThree;
+	}
+
+	@GetMapping("/cumprimentar")
+	@ResponseBody
+	public String cumprimentar() {
+		StringBuilder result = new StringBuilder();
+		result.append(helloOne.oi()).append("<br>");
+		result.append(helloTwo.oi()).append("<br>");
+		result.append(helloThree.oi()).append("<br>");
+		return result.toString();
+	}
 }
